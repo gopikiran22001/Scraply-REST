f51384f2ce8db696dfceeb380f996b4db0d57521
gopikiran <gopikiran22001@yahoo.com>
2026-03-18 14:51:32 +0530

    feat: Add method to find users by role in UserRepository

--- src/main/java/com/scraply/rest/repositories/UserRepository.java
+++ src/main/java/com/scraply/rest/repositories/UserRepository.java
@@ -14,6 +14,8 @@ public interface UserRepository extends JpaRepository<User, String> {
 
     Optional<User> findByEmail(String email);
 
+    List<User> findByRole(Role role);
+
     // Find all active pickers
     List<User> findByRoleAndStatus(Role role, AccountStatus status);
 
